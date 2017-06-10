1fae9a93851d6ad51793f4d96ce75e0f738bffc7
Bruno Drzanic <bruno@q-alliance.com>
2017-06-10 17:35:23 +0200

    init commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+/**
+ * Main app class
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/dialog.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by brunekninja on 10/06/2017.
+ */
+public class dialog {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JList type;
+    private JTable detailedlist;
+}
